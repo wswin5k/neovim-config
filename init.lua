93cc691ad5ce04c6ed2e92c431ff57d378d888b3
wswin <wswin@protonmail.com>
2025-12-08 20:27:28 +0100

    Add telescope plugin

--- init.lua
+++ init.lua
@@ -9,4 +9,8 @@ vim.opt.mouse = ""
 
 vim.keymap.set("n", "nn", "<Cmd>Neotree toggle<CR>")
 
+vim.keymap.set("n", "ff", "<Cmd>Telescope find_files<CR>")
+vim.keymap.set("n", "fg", "<Cmd>Telescope live_grep<CR>")
+
 require("config.lazy")
+
