a1661717a01f09d5841232ffd823f6cac279ea93
wswin <wswin@protonmail.com>
2025-12-08 23:01:37 +0100

    Change toggle neo-tree shortcut

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.opt.number = true
 
 vim.opt.mouse = ""
 
-vim.keymap.set("n", "nn", "<Cmd>Neotree toggle<CR>")
+vim.keymap.set("n", "<leader>nn", "<Cmd>Neotree toggle<CR>")
 
 vim.keymap.set("n", "ff", "<Cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "fg", "<Cmd>Telescope live_grep<CR>")
