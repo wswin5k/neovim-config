5e63c365e435a3b490e1f386b4e55b7f1b8b689d
wswin <wswin@protonmail.com>
2025-12-08 20:26:10 +0100

    Update neo-tree config

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,10 +7,23 @@ return {
       "MunifTanjim/nui.nvim",
       "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
+    --[[ config = function()
+        require("neo-tree").setup({
+          window = {
+            width = 10
+          }
+        })
+    end, ]]
     lazy = false, -- neo-tree will lazily load itself
     opts = {
         close_if_last_window = true,
+        filesystem = {
+          use_libuv_file_watcher = true,
+        },
+        window = {
+          width = 25
+        }
     },
-    }
+  }
 }
 
