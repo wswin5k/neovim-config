924e034b3bce94575fe4519f3ece6ba6765317eb
wswin <wswin@protonmail.com>
2025-12-15 23:25:10 +0100

    Copy-paste LSP config

--- init.lua
+++ init.lua
@@ -9,8 +9,21 @@ vim.opt.mouse = ""
 
 vim.keymap.set("n", "<leader>nn", "<Cmd>Neotree toggle<CR>")
 
+--[[ vim.lsp.config["rust-analyzer"] = {
+  -- Command and arguments to start the server.
+  cmd = { "rust-analyzer" },
+  -- Filetypes to automatically attach to.
+  filetypes = { "rust" },
+  -- Sets the "workspace" to the directory where any of these files is found.
+  -- Files that share a root directory will reuse the LSP server connection.
+  -- Nested lists indicate equal priority, see |vim.lsp.Config|.
+  root_markers = { ".git" },
+}
+vim.lsp.enable("rust-analyzer") ]]
+
+
 vim.keymap.set("n", "ff", "<Cmd>Telescope find_files<CR>")
 vim.keymap.set("n", "fg", "<Cmd>Telescope live_grep<CR>")
 
 require("config.lazy")
-
+require("config.lsp")
